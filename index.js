9d0e059fed731fe6239a043ec83b7ef598294abf
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-09-09 09:25:42 +0300

    complited assinment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (accumulator, num) => accumulator+= num;
+
+const totalBatteries = batteryBatches.reduce(reducer, 0);
+
+console.log(totalBatteries);
